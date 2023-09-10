29b9ef51d14b46442b7f9f1b8390c9e6305f4cea
diyahir <diyahirc@gmail.com>
2023-09-09 22:54:36 -0500

    Pretty

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -1,21 +1,27 @@
-import {Button, Flex} from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 
-import {Link} from '@chakra-ui/next-js'
+import { Link } from "@chakra-ui/next-js";
 
 export function Buttons() {
-    return (
-        <Flex direction={"row"} gap={"2"} padding={"2"}>
-            <Link href='/mint' color='blue.400' _hover={{color: 'blue.500'}}>
-                <Button colorScheme='blue'>Mint</Button>
-            </Link>
+  return (
+    <Flex
+      width={"100%"}
+      justifyContent={"center"}
+      direction={"row"}
+      gap={"2"}
+      padding={"2"}
+    >
+      <Link href="/mint" color="blue.400" _hover={{ color: "blue.500" }}>
+        <Button colorScheme="blue">Mint</Button>
+      </Link>
 
-            <Link href='/redeem' color='blue.400' _hover={{color: 'blue.500'}}>
-                <Button colorScheme='blue'>Redeem</Button>
-            </Link>
+      <Link href="/redeem" color="blue.400" _hover={{ color: "blue.500" }}>
+        <Button colorScheme="blue">Redeem</Button>
+      </Link>
 
-            <Link href='/redeem' color='blue.400' _hover={{color: 'blue.500'}}>
-                <Button colorScheme='blue'>Receive</Button>
-            </Link>
-        </Flex>
-    )
+      <Link href="/redeem" color="blue.400" _hover={{ color: "blue.500" }}>
+        <Button colorScheme="blue">Receive</Button>
+      </Link>
+    </Flex>
+  );
 }
